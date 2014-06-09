0fb870858cf04446987fd24f3bd08684a6aa7017
Nicolas Cortot <nc@rznc.net>
2014-06-09 23:03:17 +0200

    LED1642GW setup using timer 2

--- main/src/led.cpp
+++ main/src/led.cpp
@@ -2,11 +2,11 @@
 
 #include "led.h"
 
-#define SDO_PORT    PORTB
+#define SDO_PORT    PORTC
 #define SDO_PIN     1
-#define CLK_PORT    PORTB
+#define CLK_PORT    PORTC
 #define CLK_PIN     2
-#define LATCH_PORT  PORTB
+#define LATCH_PORT  PORTC
 #define LATCH_PIN   3
 
 #define PWM_PIN     4
@@ -32,28 +32,36 @@ void led_setup()
     LATCH_PORT &= ~_BV(LATCH_PIN);
 
     // Enable output pins
+    DDRC |= _BV(SDO_PIN) | _BV(CLK_PIN) | _BV(LATCH_PIN);
 
     // LED1642GW configuration
     write(0x0000, CMD_SWITCH);
     write(0x0400, CMD_CONFIG);
 
-    // Enable PWM signal usin timer 2
-
+    // Enable PWM clock using timer 2
+    // Toggle OC2B on compare match, CTC mode
+    TCCR2A = _BV(COM2B0) | _BV(WGM21);
+    // No prescaling
+    TCCR2B = _BV(CS20);
+    // Set timer TOP to the lowest value for f = F_CPU / 2
+    OCR2A = 0;
+    // Enable OC2B output
+    DDRD |= _BV(3);
 
     // Testing
-    write(0xff, CMD_SWITCH);
+    write(0xffff, CMD_SWITCH);
     for (uint8_t channel = 0; channel < CHANNELS; ++channel) {
-        brightness[channel] = (16 - channel) * 4000;
+        brightness[channel] = 0xffff - channel * 0x1000;
     }
+    for (uint8_t channel = CHANNELS - 1; channel > 0; --channel) {
+        write(brightness[channel], CMD_CHANNEL);
+    }
+    write(brightness[0], CMD_GLOBAL);
 }
 
 
 void led_loop()
 {
-    for (uint8_t channel = CHANNELS - 1; channel > 0; --channel) {
-        write(brightness[channel], CMD_CHANNEL);
-    }
-    write(brightness[0], CMD_GLOBAL);
 }
 
 
