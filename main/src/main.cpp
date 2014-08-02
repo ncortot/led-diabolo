1aeb7fc689f24b28753abf380c4c76d68cd660fc
Nicolas Cortot <nc@rznc.net>
2014-08-02 19:10:21 +0200

    Adding IRLib

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,25 +1,20 @@
 #include <Arduino.h>
 
+#include <IRLib.h>
 #include <led.h>
 
 static uint16_t counter = 0;
+IRsend irsend;
 
 void setup() {
     Serial.begin(19200);
-
-    pinMode(A0, OUTPUT);
-    pinMode(A1, OUTPUT);
-    pinMode(A2, OUTPUT);
-
     led_setup();
 }
 
 void loop() {
     ++counter;
 
-    digitalWrite(A0, counter & 0x1 ? HIGH : LOW);
-    digitalWrite(A1, counter & 0x2 ? HIGH : LOW);
-    digitalWrite(A2, counter & 0x4 ? HIGH : LOW);
+    irsend.send(SONY,0xa8bca, 20);
 
     Serial.print("loop ");
     Serial.println(counter, HEX);
